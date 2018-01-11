423c1521ab15a09b765df1e610c42df3306189a0
Erik Rintala <gerfiglmac@gmail.com>
2018-01-10 20:37:28 -0800

    ex04

---
+++ d01/ex04/ex04.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string    &ref = str;
+    std::string    *ptr = &str;
+    std::cout << *ptr << std::endl;
+    std::cout << ref << std::endl;
+	return 0;
+}
